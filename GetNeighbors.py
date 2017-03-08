88b25da0a3ae793a622ae7d7beee35decfa67556
regireed89 <regireed89@gmail.com>
2017-03-08 16:44:48 -0600

    first try

---
+++ GetNeighbors.py
@@ -0,0 +1,29 @@
+import os
+import random
+import sys
+
+class Node(object):
+    letter = ""
+    def __init__(self, l)
+        self.letter = l
+
+
+
+
+
+class Graph(object):
+    
+    graph = [3][3];
+
+    def __init__(self,g)
+    self.graph = g
+    
+
+
+
+    
+
+    def GetNeighbors(node, graph):
+        return list;
+
+
