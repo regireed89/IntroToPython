1d7bef7299fbd976b6ca83f2f5949e75688b2b7a
regireed89 <regireed89@gmail.com>
2017-03-09 12:56:21 -0600

    py

--- GetNeighbors.py
+++ GetNeighbors.py
@@ -8,25 +8,40 @@ class Node(object):
         self.identifier = identifier
         self.value = value
 
+    def print_info():
+        print self.value
+        print self.identifier
+
+
 
 class Graph(object):
 
     def __init__(self,nodes):
-        self.nodes = nodses
+        self.nodes = nodes
 
     def GetNeighbors(node, graph):
-        return list;
+        
+        return list
 
-    def makenodes():
-    nodes = []
-    for i in range(0,25):
-        node = Node(i*i,i)
-        nodes.append(node)
+    def MakeNodes():
+         for i in range(0,25):
+            node = Node(i*i,i)
+            nodes.append(node)
     graph = Graph(nodes)
     gotnode = graph.GetNeighbors(10)
-    gotnode.print_info()    
+    gotnode.print_info() 
+    
+      
 
+    
+        
+def GetNode(node, graph):
+    
 
+A = Node(1, "A")
+B = Node(2, "B")
+C = Node(3, "C")
+D = Node(4, "D")
 
-A = Node(0,"A")
-B = Node(1, "B")
+mymap = [A,B,C,D]
+print A.print_info()
