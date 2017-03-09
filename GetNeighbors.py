51ed3f7684ef3ca09642c5d4fb86adbe220de0bf
regireed89 <regireed89@gmail.com>
2017-03-08 18:04:51 -0600

    getting neighbors

--- GetNeighbors.py
+++ GetNeighbors.py
@@ -3,27 +3,28 @@ import random
 import sys
 
 class Node(object):
+    
     letter = ""
-    def __init__(self, l)
-        self.letter = l
-
-
-
+    def __init__(self, value, identifier):
+        self.identifier = identifier
+        self.value = value
 
 
 class Graph(object):
-    
-    graph = [3][3];
 
-    def __init__(self,g)
-    self.graph = g
-    
-
-
-
+    def __init__(self,g):
+        self.nodes = nodses
     
 
     def GetNeighbors(node, graph):
         return list;
 
+def makenodes():
+    nodes = []
+    for i in range(0,25):
+        node = Node(i*i,i)
+        nodes.append(node)
+    graph = Graph(nodes)
+    gotnode = graph.GetNeighbors(10)
+    gotnode.print_info()    
 
