ce23e56e372ed5eeff2eefc20659ee14219d09d2
regireed89 <regireed89@gmail.com>
2017-03-10 13:58:22 -0600

    game

---
+++ Game.py
@@ -0,0 +1,60 @@
+# Import a library of functions called 'pygame'
+import pygame
+from math import pi
+
+# Initialize the game engine
+pygame.init()
+
+# Define the colors we will use in RGB format
+BLACK = (0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE = (0,   0, 255)
+GREEN = (0, 255,   0)
+RED = (255,   0,   0)
+
+# Set the height and width of the screen
+size = [400, 300]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Example code for the draw module")
+
+# Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+point = [(200, 0), (0, 150)]
+
+
+while not done:
+
+    # This limits the while loop to a max of 10 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(10)
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+
+    # All drawing code happens after the for loop and but
+    # inside the main while done==False loop.
+
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+
+    # Draw a rectangle outline
+    pygame.draw.rect(screen, BLACK, [75, 10, 50, 20], 2)
+
+    # Draw a solid rectangle
+    pygame.draw.rect(screen, BLACK, [150, 10, 50, 20])
+
+    # Draw a circle
+    pygame.draw.circle(screen, BLUE, [60, 250], 40)
+
+    for i in point:
+        pygame.draw.circle(screen, BLACK, point, 20)
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
+    pygame.display.flip()
+
+
+# Be IDLE friendly
+pygame.quit()
--- MorningAssignment.py
+++ MorningAssignment.py
@@ -5,13 +5,17 @@ import random
 class Player(object):
     '''da player'''
 
-    def __init__(self, cardtotal):
+    def __init__(self, name, cardtotal):
+        self.name = name
         self.cardtotal = cardtotal
 
 
-__player1__ = Player(0)
-__player2__ = Player(0)
-__Dembois__ = [__player1__, __player2__]
+__player1__ = Player("bob", 0)
+__player2__ = Player("jim", 0)
+__player3__ = Player("carl", 0)
+__player4__ = Player("regi", 0)
+__Dembois__ = [__player1__, __player2__, __player3__, __player4__]
+__current__ = Player("n\a", 0)
 
 
 def hightotal(mylist):
@@ -21,8 +25,10 @@ def hightotal(mylist):
         for i in range(5):
             i = random.choice([5, 10])
             play.cardtotal += i
-            if play.cardtotal > play.cardtotal:
-                return play, play.cardtotal
+            if __current__.cardtotal < play.cardtotal:
+                __current__.name = play.name
+                __current__.cardtotal = play.cardtotal
+        print __current__.name, __current__.cardtotal
 
 
 if __name__ == '__main__':
