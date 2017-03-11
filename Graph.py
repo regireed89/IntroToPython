0b3b4bc9368db6531429413052cb5ed723538ef6
regireed89 <regireed89@gmail.com>
2017-03-10 18:13:33 -0600

    end of day

--- Graph.py
+++ Graph.py
@@ -27,31 +27,32 @@ class Graph(object):
 
     def get_node(self, node, graph):
         '''gets node'''
+        @classmethod
         if node == graph.node:
             return graph.node[node]
 
 
 def get_neighbors(node, graph):
     '''gets nodes neighbors'''
-    graph.node[node] = graph.node[node] + 1
+    graph.node[node] = graph.node[node.x] + 1
     if graph.node[node] is None:
         return
     else:
         return graph.node[node]
 
-    graph.node[node] = graph.node[node] + 1 
+    graph.node[node] = graph.node[node.y] + 1
     if graph.node[node] is None:
         return
     else:
         return graph.node[node]
-            
+
 
 def test_nodes():
     '''test the nodes'''
     graph = Graph(3, 3)
     node = graph.get_node(2, graph)
     node.print_info()
-    nod = get_neighbors(2,graph)
+    nod = get_neighbors(2, graph)
     nod.print_info()
 
 
